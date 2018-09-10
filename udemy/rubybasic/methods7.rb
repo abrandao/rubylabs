17acf4a3ab5136e730d99e471f8a0b031cfc3662
abrandao <andersonbrandaolustosa@gmail>
2018-09-10 15:55:05 -0300

    Method files reorganized

---
+++ udemy/rubybasic/methods7.rb
@@ -0,0 +1,20 @@
+## Interroga se a variável está vazia
+.nil?
+
+## Copiando uma variável
+.dup
+
+## Congelando um objeto
+.freeze
+
+## Procurando letras
+.scan
+
+## Verificando se um objeto suporta determinado método
+.respond_to?
+
+## Convertendo um range em array
+.to_a
+
+## Conversões de bases
+
