76e73163393690b1335358bdfec3a72eed2f97ad
abrandao <andersonbrandaolustosa@gmail>
2018-09-10 19:17:09 -0300

    new method lessons

---
+++ udemy/rubybasic/arrays1.rb
@@ -0,0 +1,26 @@
+#### Métodos para arrays parte 1 #####
+
+## unshift 
+#Adiciona elemento no início do array
+
+
+## shift
+#Remove elemento do início do array
+
+
+## pop
+# Remove o último elemento do array
+
+
+## slice 
+# copia partes de um array
+
+
+
+## interseção
+
+
+## soma
+
+
+## multiplicação
--- udemy/rubybasic/methods7.rb
+++ udemy/rubybasic/methods7.rb
@@ -1,20 +1,55 @@
 ## Interroga se a variável está vazia
-.nil?
+#.nil?
+v = nil
+#puts v.nil?
 
 ## Copiando uma variável
-.dup
+#.dup
+=begin
+nome = "Artur"
+outro_nome = nome
+puts outro_nome
+puts nome[0] = "x"
+puts nome
+puts outro_nome
+
+nome = "Artur"
+outro_nome = nome.dup
+
+nome[0] = "x"
+puts nome
+puts outro_nome
+=end
 
 ## Congelando um objeto
-.freeze
+#.freeze
+=begin
+nome = "Artur"
+nome.freeze 
+nome[0] = "x"
+puts nome
+=end
 
 ## Procurando letras
-.scan
+#.scan
+=begin
+str = "palavras"
+a = str.scan("a")
+puts a.size
+
+b = str.scan(/./)
+puts b.inspect
+=end
 
 ## Verificando se um objeto suporta determinado método
-.respond_to?
+#.respond_to?
+#puts 1.respond_to?(:to_s)
 
 ## Convertendo um range em array
-.to_a
+#.to_a
+print (1..10).to_a
+puts
 
 ## Conversões de bases
-
+#De inteiro para binário
+puts 20.to_s(2)
