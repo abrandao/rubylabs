efd0a3a5be52159788c768ac2be46ee7c3eee559
abrandao <andersonbrandaolustosa@gmail.com>
2018-10-16 14:27:09 -0300

    seeds.rb
    Gem faker

--- studies/udemy/Rails4/notebook/db/seeds.rb
+++ studies/udemy/Rails4/notebook/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "Gerando os tipos de contato (Kinds)..."
+Kind.create!([{description: "Amigo"},
+                    {description: "Contato"}, {description: "Comercial"}])
+puts "Tipos gerados"
+
+puts "Gerando os Contacts..."
+10.times do |i|
+  Contact.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    kind: Kind.all.sample,
+    rmk: Faker::Lorem.paragraph([1, 2, 3, 4, 5].sample)
+  )
+end
+puts "Contatos gerados"
