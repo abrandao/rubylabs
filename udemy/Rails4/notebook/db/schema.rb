270c7f552d1074a3d513ba9aeec5dcc594f2cc7e
abrandao <andersonbrandaolustosa@gmail.com>
2018-10-22 18:11:09 -0300

    Select field for states

--- udemy/Rails4/notebook/db/schema.rb
+++ udemy/Rails4/notebook/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181018145621) do
+ActiveRecord::Schema.define(version: 20181022201715) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "street"
