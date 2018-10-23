2ee9a063fdeb3465d47a75a457dc22de27368c0b
abrandao <andersonbrandaolustosa@gmail.com>
2018-10-23 11:43:06 -0300

    Organizing I18N

--- udemy/Rails4/notebook/db/schema.rb
+++ udemy/Rails4/notebook/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181022201715) do
+ActiveRecord::Schema.define(version: 20181023131804) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "street"
