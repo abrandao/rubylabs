24803aef15d55a2a3a74e8816d7c5cee998a6249
abrandao <andersonbrandaolustosa@gmail.com>
2018-11-01 16:29:53 -0300

    pomodorojs Global Variables

--- udemy/pomodorojs/js/main.js
+++ udemy/pomodorojs/js/main.js
@@ -1,1 +1,9 @@
-//pomodoro
+//Global Variables
+
+var workClicks = 25;
+var breakClicks = 5;
+var workDisplay = document.getElementById("work-dislay");
+var breakDisplay = document.getElementById("break-display");
+var count = 0;
+var workSession;
+var breakSession;
