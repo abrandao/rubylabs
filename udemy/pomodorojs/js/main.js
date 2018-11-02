791fd3317d061e911430d0cec5e24f25a6e57547
abrandao <andersonbrandaolustosa@gmail.com>
2018-11-02 12:09:01 -0300

    increase time button works

--- udemy/pomodorojs/js/main.js
+++ udemy/pomodorojs/js/main.js
@@ -2,8 +2,15 @@
 
 var workClicks = 25;
 var breakClicks = 5;
-var workDisplay = document.getElementById("work-dislay");
+var workDisplay = document.getElementById("work-display");
 var breakDisplay = document.getElementById("break-display");
 var count = 0;
 var workSession;
 var breakSession;
+
+// increase work time
+var increaseWork = document.getElementById("addWorkButton");
+increaseWork.addEventListener("click", function(){
+  workClicks+= 1;
+  workDisplay.innerHTML = workClicks;
+}, false);
