10b027fa1b622bfcc9f363af1ead0b029e2e489b
abrandao <andersonbrandaolustosa@gmail.com>
2018-11-06 09:22:19 -0200

    function reset

--- udemy/pomodorojs/js/main.js
+++ udemy/pomodorojs/js/main.js
@@ -81,3 +81,19 @@ function pause() {
 function resume() {
   workSession = setInterval(workCountDown, 1000);
 } //end of function
+
+
+// function reset
+function reset() {
+  if(workSession) {
+    clearInterval(workSession);
+    workSession = null;
+  } else {
+    clearInterval(breakSession);
+    breakSession = null;
+  }
+  document.getElementById("showtime").innerHTML = "";
+  document.getElementById("timer-panel").style.backgroundColor = "#FC5D66";
+  document.getElementById("pause").disabled = false;
+  document.getElementById("resume").disabled = false;
+} // end of function
