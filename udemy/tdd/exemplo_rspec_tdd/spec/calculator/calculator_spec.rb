cdf543ea908fa7eecb01209b7d05e49d50e9c3f1
abrandao <andersonbrandaolustosa@gmail.com>
2018-11-14 11:57:12 -0200

    1st test complete
    with 2 methods

--- udemy/tdd/exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
+++ udemy/tdd/exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -1,5 +1,15 @@
 require 'calculator'
 
 describe Calculator do
+  it 'sum method for 2 numbers' do
+    calc = Calculator.new
+    result = calc.sum(5,6)
+    expect(result).to eq(11)
+  end
   
+  specify 'sum method for 2 negative numbers' do
+    calc = Calculator.new
+    result = calc.sum(-5,7)
+    expect(result).to eq(2)
+  end
 end
