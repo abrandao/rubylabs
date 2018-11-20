50eef6708385eb5555b252b540079b18df894568
abrandao <andersonbrandaolustosa@gmail.com>
2018-11-20 16:10:09 -0200

    delete tobetjs/

--- tobetjs/js/main.js
+++
@@ -1,7 +0,0 @@
-var increaseBreak = document.getElementById("transfer-Button");
-increaseBreak.addEventListener("click", function(){
-
-  var transferValue = document.getElementById("receive-Value").value;
-  document.getElementById("show-Value").innerHTML = transferValue;
-  
-}, false);
