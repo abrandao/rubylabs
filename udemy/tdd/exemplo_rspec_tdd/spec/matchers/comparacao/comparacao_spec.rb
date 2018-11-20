3c420a939533cef1487eb1d3e74250c49cf664de
abrandao <andersonbrandaolustosa@gmail.com>
2018-11-20 10:30:19 -0200

    matchers de comparação

--- udemy/tdd/exemplo_rspec_tdd/spec/matchers/comparacao/comparacao_spec.rb
+++ udemy/tdd/exemplo_rspec_tdd/spec/matchers/comparacao/comparacao_spec.rb
@@ -1,28 +1,47 @@
-describe 'Matchers de comparação' do
-  it '#equal' do
-    x = "ruby"
-    y = "ruby"
-    expect(x).not_to equal(y)
-    expect(x).to equal(x)
+describe 'Matchers de Comparação' do
+  
+  it '>' do
+    expect(5).to be > 1
   end
 
-  it '#be - Testa se é o mesmo objeto' do
-    x = "ruby"
-    y = "ruby"
-    expect(x).not_to be(y)
-    expect(x).to be(x)
+  it '>=' do
+    expect(5).to be >= 2
+    expect(5).to be >= 5
   end
 
-  it '#eql - Testa o valor/conteúdo' do
-    x = "ruby"
-    y = "ruby"
-    expect(x).to eql(y)    
+  it '<' do
+    expect(5).to be < 10
   end
 
-  it '#eq - Versão curta do eql' do
-    x = "ruby"
-    y = "ruby"
-    expect(x).not_to equal(y)
-    expect(x).to eq(x)
+  it '<=' do
+    expect(5).to be <= 12
+    expect(5).to be <= 5
   end
+
+  it 'be_between inclusive' do
+    expect(5).to be_between(2,7).inclusive
+    expect(2).to be_between(2,7).inclusive
+    expect(7).to be_between(2,7).inclusive
+  end
+
+  it 'be_between exclusive' do
+    expect(5).to be_between(2,7).exclusive
+    expect(3).to be_between(2,7).exclusive
+    expect(6).to be_between(2,7).exclusive
+  end
+
+  it 'match' do
+    expect("fulano@fulano.com.br").to match(/..@../)
+  end
+
+  it 'start_with' do
+    expect("fulano de tal").to start_with("fulano")
+    expect([1, 2, 3]).to start_with(1)
+  end
+
+  it 'end_with' do
+    expect("fulano de tal").to end_with("tal")
+    expect([1, 2, 3]).to end_with(3)
+  end
+
 end
