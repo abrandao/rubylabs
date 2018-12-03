52c2fb32cac4161594fbe2ab0fb244cbb44fdf3d
abrandao <andersonbrandaolustosa@gmail.com>
2018-12-03 09:41:28 -0200

    on-liner syntax

--- udemy/tdd/exemplo_rspec_tdd/spec/matchers/ranges/ranges_spec.rb
+++ udemy/tdd/exemplo_rspec_tdd/spec/matchers/ranges/ranges_spec.rb
@@ -4,4 +4,11 @@ describe (1..5), 'Ranges' do
     expect(subject).to cover(2,5)
     expect(subject).not_to cover(0,6)
   end
+
+  # for best description of the test on result
+  it { is_expected.to cover(2) }
+  it { is_expected.to cover(2, 5) }
+  it { is_expected.not_to cover(0, 6) }
+
+  it { expect(subject).to cover(2) }
 end
