393ececf4f2c480efbb2cb248f7dfeb73ba4d634
abrandao <andersonbrandaolustosa@gmail.com>
2018-12-13 09:45:47 -0200

    Variáveis

---
+++ bootcamp/modulo01/variaveis.rb
@@ -0,0 +1,22 @@
+#declarar locais, instancia, globais e uma constante
+
+#declarar locais, instancia, globais e uma constante
+
+#declara int
+num = 5 #variavel local
+
+#declara float
+@num2 = 3.5 #variavel de instancia
+
+#declara string
+cor = 'verde'
+
+#declara boolean
+$teste = true #variável global
+
+#declara nil
+@teste2 = nil
+
+#declara array
+
+TESTE = [1, 2, 3] #constante
