364704900cabb59fe4d68b2a3bd40bb90bdce04e
abrandao <andersonbrandaolustosa@gmail.com>
2018-12-13 09:53:53 -0200

    Strings

---
+++ bootcamp/modulo01/strings/strings.rb
@@ -0,0 +1,20 @@
+#declaração de um string
+hello = "Olá"
+world = " Mundo!"
+ 
+#concatenar
+puts hello + world
+#interpolar
+puts "#{hello} #{world}"
+# append usando &lt;&lt;
+string = "Olá"
+string &lt;&lt; " Mundo!"
+ 
+puts string
+ 
+#Método capitalize e capitalize!
+ 
+#ver metodos no console
+# String.methods.sort
+ 
+#Visite https://ruby-doc.org/core-2.4.2/String.html
