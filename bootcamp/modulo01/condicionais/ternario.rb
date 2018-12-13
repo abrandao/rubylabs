ad1f609b70066e6c796d58218e8cb7692231c1ce
abrandao <andersonbrandaolustosa@gmail.com>
2018-12-13 12:19:27 -0200

    Ruby Range Example

--- bootcamp/modulo01/condicionais/ternario.rb
+++ bootcamp/modulo01/condicionais/ternario.rb
@@ -5,10 +5,26 @@
  
 puts (5 > 4 ) ? "5 é maior que 4" : "5 não é maior que 4"
  
-puts "Digite sua idade :)"
+puts "Digite sua idade: "
 age = gets.chomp.to_i #método pra pegar uma resposta do usuario no console
  
 #Fazer um ternário se o usuário é adolescente, adulto ou idoso.
 #macete do ternário com mais de 2 condições
 
-puts (age < 18) ? "Você é um adolescente" : (age < 60) ? "Você é um adulto" : "Você é um idoso"
+puts age < 18 ? "Você é um adolescente" : age < 60 ? "Você é um adulto" : "Você é um idoso"
+
+puts "------------------------"
+
+puts "Mesmo exemplo usando Ruby Ranges"
+
+puts
+
+result = case age
+   when 0..12 then "Criança"
+   when 13..18 then "Adolescente"
+   when 19..60 then "Adulto"
+   when 61..120 then "Idoso"
+   else "Valor inválido"
+end
+
+puts result
