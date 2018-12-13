894492e2185f93206642957b9eab084f18e1ded6
abrandao <andersonbrandaolustosa@gmail.com>
2018-12-13 09:31:48 -0200

    First program

---
+++ bootcamp/modulo01/meu_primeiro_programa/meu_primeiro_programa.rb
@@ -0,0 +1,17 @@
+#Imprimindo na tela
+puts 'Olá mundo'
+puts 5 + 3
+ 
+#Atribuindo valor à váriavel
+nome = 'João'
+cor = "azul"
+numero = 10
+#Concatenando variável
+puts 'Olá ' + nome + '. Bem vindo ao mundo Ruby =D'
+puts nome + 'teste' #Joãoteste
+puts "#{nome} teste" #João teste
+ 
+#Fazendo interpolação de variável
+puts "Olá #{nome}. Bem vindo ao mundo Ruby =D"
+ 
+puts 'e'.class
