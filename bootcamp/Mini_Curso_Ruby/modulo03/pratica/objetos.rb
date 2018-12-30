39516b1fdd44162157786019fe2b3fa27fa01235
abrandao <andersonbrandaolustosa@gmail.com>
2018-12-30 11:07:13 -0200

    Practial example part 02

--- bootcamp/Mini_Curso_Ruby/modulo03/pratica/objetos.rb
+++ bootcamp/Mini_Curso_Ruby/modulo03/pratica/objetos.rb
@@ -1,6 +1,13 @@
 require_relative "person"
+require_relative "atleta"
 
 pessoa1 = Person.new("Christi Allan", 23, 1.75, "homem")
 pessoa2 = Person.new("Maria", 25, 1.63, "mulher")
 pessoa1.introduce
 pessoa2.introduce
+puts "-------------------------------------"
+atleta = Atleta.new("Cristi", 23, 1.75, "homem", "Jiu-Jitsu")
+atleta.introduce
+atleta.set_sport("natação")
+atleta.introduce
+atleta.my_sport
