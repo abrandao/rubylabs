816183ff4854ccf6e4a20b7ce06ffeabee43f50b
abrandao <andersonbrandaolustosa@gmail.com>
2019-02-13 16:53:06 -0200

    convert while typing
    not working yet

--- testes/change_tag_js/scripttest.js
+++ testes/change_tag_js/scripttest.js
@@ -1,6 +1,5 @@
-//function myFunction() {
+var textInput = $('#amount').val();
   
-  var textInput = document.getElementById('myText');
   var timeout = null;
 
   textInput.onkeyup = function (e) {
@@ -8,16 +7,25 @@
     clearTimeout(timeout);
 
     timeout = setTimeout(function () {
-      //alert(textInput.value);
-      cot = 3;
-      var x = document.getElementById("myText").value;
-      document.getElementById("demo").value = x * cot; 
+      alert(textInput);
+      if ($('form').attr('action') === '/convert') {
+        $.ajax('/convert', {
+          type: 'GET',
+          dataType: 'json',
+          data: {
+            source_currency: $("#source_currency").val(),
+            target_currency: $("#target_currency").val(),
+            amount: $("#amount").val()
+          },
+          error: function(jqXHR, textStatus, errorThrown) {
+            return alert(textStatus);
+          },
+          success: function(data, text, jqXHR) {
+            return $('#result').val(data.value);
+          }
+        });
+        return false;
+      }
     }, 1000);
+
   }
-
-
-  //cot = 3;
-  //var x = document.getElementById("myText").value;
-  //document.getElementById("demo").value = x * cot;  
-
-//}
