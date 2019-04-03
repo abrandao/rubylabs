f2116be51e795e411ed065748bd8d76154221172
abrandao <andersonbrandaolustosa@gmail.com>
2019-04-03 11:13:11 -0300

    translation_bot_api test basic structure

--- testes/change_tag_js/scripttest.js
+++ testes/change_tag_js/scripttest.js
@@ -1,31 +1,14 @@
-var textInput = $('#amount').val();
-  
-  var timeout = null;
+var textInput = document.getElementById('myText');
+var timeout = null;
 
-  textInput.onkeyup = function (e) {
-    
-    clearTimeout(timeout);
+textInput.onkeyup = function (e) {
 
-    timeout = setTimeout(function () {
-      alert(textInput);
-      if ($('form').attr('action') === '/convert') {
-        $.ajax('/convert', {
-          type: 'GET',
-          dataType: 'json',
-          data: {
-            source_currency: $("#source_currency").val(),
-            target_currency: $("#target_currency").val(),
-            amount: $("#amount").val()
-          },
-          error: function(jqXHR, textStatus, errorThrown) {
-            return alert(textStatus);
-          },
-          success: function(data, text, jqXHR) {
-            return $('#result').val(data.value);
-          }
-        });
-        return false;
-      }
-    }, 1000);
+  clearTimeout(timeout);
 
-  }
+  timeout = setTimeout(function () {
+    //alert(textInput.value);
+    cot = 3;
+    var x = document.getElementById("myText").value;
+    document.getElementById("demo").value = x * cot; 
+  }, 1000);
+}
