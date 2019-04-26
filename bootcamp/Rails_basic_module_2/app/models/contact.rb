142867184525c8c5aaf88aace690d7cc59b96e25
abrandao <andersonbrandaolustosa@gmail.com>
2019-04-26 09:28:18 -0300

    Basic structure

--- bootcamp/Rails_basic_module_2/app/models/contact.rb
+++ bootcamp/Rails_basic_module_2/app/models/contact.rb
@@ -1,2 +1,7 @@
 class Contact < ApplicationRecord
+  has_many :phones # possui muitos telefones
+  has_one :address # possui um único endereço
+
+  has_many :contact_kinds # contact possui muitos kinds
+  has_many :kinds, through: :contact_kinds # contact possui muitos kinds
 end
--- bootcamp/Rails_basic_module_2/app/models/kind.rb
+++ bootcamp/Rails_basic_module_2/app/models/kind.rb
@@ -1,2 +1,4 @@
 class Kind < ApplicationRecord
+  has_many :contact_kinds # contact possui muitos kinds
+  has_many :kinds, through: :contact_kinds # contact possui muitos kinds
 end
