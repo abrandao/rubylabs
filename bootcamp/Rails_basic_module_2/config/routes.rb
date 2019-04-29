50505fb411293fe8a30bf19323cff0f8fa1a2460
abrandao <andersonbrandaolustosa@gmail.com>
2019-04-29 09:33:05 -0300

    basic crud controllers, routes and views

--- bootcamp/Rails_basic_module_2/config/routes.rb
+++ bootcamp/Rails_basic_module_2/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'contacts/index'
+  get 'contacts/show'
+  get 'contacts/new'
+  post 'contacts/create'
+  get 'contacts/edit'
+  put 'contacts/update'
+  delete 'contacts/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
