3c26a210b43e3fe6a83f48e582894c14a2cb3373
abrandao <andersonbrandaolustosa@gmail.com>
2019-05-21 10:34:40 -0300

    Edit contacts

--- bootcamp/Rails_basic_module_2/config/routes.rb
+++ bootcamp/Rails_basic_module_2/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root 'contacts#index'
-  #resources :contacts
+  resources :contacts
   get 'contacts/show'
   get 'contacts/new'
   post 'contacts/create'
-  get 'contacts/edit'
+  
   put 'contacts/update'
   delete 'contacts/destroy' => 'contacts#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
