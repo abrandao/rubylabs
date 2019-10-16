496b6d74903455cb5af5444e744d539727958925
abrandao <andersonbrandaolustosa@gmail.com>
2019-10-16 20:48:31 -0300

    Estruturas de repetição

---
+++ udemy/poo_ruby/ola/08-estruturas-de-repeticao.rb
@@ -0,0 +1,14 @@
+i = 1
+while i <= 10 # Enquanto for verdadeiro
+  puts "Anderson - #{i}"
+  i += 1
+end
+
+# i == 50 
+
+until i <= 0 # Enquanto for falso
+  puts "Sou falso - #{i}"
+  i -= 1
+end
+
+puts "Fim!"
