17780eb4bb79bf0587bac5a63419375370e81057
abrandao <andersonbrandaolustosa@gmail.com>
2019-11-03 12:17:14 -0300

    Deafio Lógica de Programação - Etapa #2 Iniciando

---
+++ bootcamp/logica_de_programacao/jogo_da_velha.js
@@ -0,0 +1,25 @@
+/* 
+JOGO DA VELHA
+
+Desenvolva um pequeno programa que consiga ao receber um placar de jogo da velha (completo ou incompleto) analisar se um dos jogadores venceu ou se houve um empate.
+
+FORMATO DA ENTRADA DE DADOS:
+['X','O','O'],['X','X','O'],['O','X','O']
+
+SAÍDA DESEJADA:
+'O vencedor é o jogador que usou X' ou 'O jogo empatou'.
+*/
+
+console.log('hello2')
+
+array = [['X','O','O'],['X','X','O'],['O','X','O']]
+
+console.log("=======================")
+console.log(array)
+console.log(array[0])
+console.log(array[1])
+console.log(array[2])
+console.log(array[0][2])
+
+
+// [0['0','1','2'],[1['0','1','2'],[2['0','1','2']]
