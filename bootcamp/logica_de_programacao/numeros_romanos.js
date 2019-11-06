cb64eb447a6e9d18ea88d1682bb195fc7c420ab6
abrandao <andersonbrandaolustosa@gmail.com>
2019-11-06 19:13:12 -0300

    Desafio Lógica de Programação - Etapa #3 Finalizado

--- bootcamp/logica_de_programacao/numeros_romanos.js
+++ bootcamp/logica_de_programacao/numeros_romanos.js
@@ -7,37 +7,21 @@
 
 # SAÍDA DESEJADA:
 - CMXXX
-
-# REFERÊNCIA:
-- https://fazaconta.com/calculadora-romanos.htm
 */
 
-var num = 22;
-
-// storing roman values of digits from 0-9
-// when placed at different places 
+// storing roman values of digits from 0-9 when placed at different places 
 var m = ["", "M", "MM", "MMM"]; 
 var c = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
 var x = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
 var i = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
 
-// Converting to roman
-thousands = m[num / 1000];
-hundereds = c[(num % 1000) / 100];
-tens = x[(num % 100) / 10];
-ones = i[num % 10]; //OK
-ans = thousands + hundereds + tens + ones;
-f = (num % 100) / 10;
+var num = 39;
 
-document.write(thousands);
-document.write(hundereds);
-document.write(tens);
-document.write(ones);
-document.write(ans);
-console.log("thousands: " + thousands);
-console.log("hundereds: " + hundereds);
-console.log("tens: " + tens);
-console.log("ones: " + ones);
-console.log("ans: " + ans);
-console.log(f);
-console.log(x[2.2]);
+// Converting to roman
+thousands = m[parseInt(num / 1000)];
+hundereds = c[parseInt((num % 1000) / 100)];
+tens = x[parseInt((num % 100) / 10)];
+ones = i[num % 10];
+ans = thousands + hundereds + tens + ones;
+
+console.log("O número " + num + " equivale a " + ans + " em algarismos romanos.");
