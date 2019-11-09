1d5460c5518994d087d6b1922ca3cfcabb42b5f6
abrandao <andersonbrandaolustosa@gmail.com>
2019-11-09 20:03:08 -0300

    Jogo de adivinhação

---
+++ udemy/poo_ruby/jogos/01-adivinhar_numero.rb
@@ -0,0 +1,29 @@
+class AdivinhaNumero
+  attr_reader :numero
+  attr_reader :venceu
+
+  def initialize
+    @numero = Random.rand(1..10)
+    @venceu = false
+  end
+
+  def tentar_adivinhar(numero = 0)
+    if numero == @numero
+      @venceu = true
+      return "Você venceu!"
+    elsif numero > @numero
+      return "O número informado é muito alto..."
+    else
+      return "O número informado é baixo..."
+    end
+  end
+end
+
+jogo = AdivinhaNumero.new
+
+until jogo.venceu do
+  puts "Digite um número: "
+  numero = gets.to_i
+
+  puts jogo.tentar_adivinhar(numero)
+end
