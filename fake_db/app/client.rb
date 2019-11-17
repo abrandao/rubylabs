c1db804a72489743f8592fdefcaa204ec5488177
abrandao <andersonbrandaolustosa@gmail.com>
2019-11-17 09:45:30 -0300

    Deleting clients

--- fake_db/app/client.rb
+++ fake_db/app/client.rb
@@ -13,7 +13,7 @@ class Client
     @date = Time.now
   end
 
-  def print
+  def register
     puts "Client's name, company, age and date of register: "
     puts "#{name}, #{company}, #{age}, #{date}."
     puts "Do you want confirm the register?(Y/N)"
@@ -43,14 +43,31 @@ class Client
   def choose_client
     puts "Choose a client:"
     option = Dir.entries("app/db").sort
-    puts "==============="
-    puts "==============="
     x = 2
     while x < option.length
       puts "#{x - 1} - " + File.readlines("app/db/#{option[x]}")[0]
       x += 1
     end
     c = gets.chomp.to_i
-    puts option[c + 1]
+    puts "========================="
+    puts "::CLIENT'S PROFILE::"
+    puts "Arquivo: " + option[c + 1]
+    puts "Name: " + File.readlines("app/db/#{option[c + 1]}")[0]
+    puts "Company: " + File.readlines("app/db/#{option[c + 1]}")[1]
+    puts "Age: " + File.readlines("app/db/#{option[c + 1]}")[2]
+  end
+
+  def delete_a_client
+    puts "Choose a client to delete:"
+    option = Dir.entries("app/db").sort
+    x = 2
+    while x < option.length
+      puts "#{x - 1} - " + File.readlines("app/db/#{option[x]}")[0]
+      x += 1
+    end
+    c = gets.chomp.to_i
+    puts "You deleted the client: " + File.readlines("app/db/#{option[c + 1]}")[0]
+    File.delete("app/db/#{option[c + 1]}")
+    rescue Errno::ENOENT
   end
 end
--- fake_db/app/menu.rb
+++ fake_db/app/menu.rb
@@ -8,14 +8,16 @@ class Menu
     puts "What do you want to do?"
     puts "1 - Register a new Client"
     puts "2 - List clients"
+    puts "3 - Choose a client"
+    puts "4 - Delete a client"
 
     @option = gets.chomp.to_i
     clt = Client.new
     
     if option == 1
       clt.form
-      clt.print
-    else
+      clt.register
+    elsif option == 2 
       clt.list_clients
       puts "Do you want to choose a client?(Y/N)"
       confirmation = gets.chomp
@@ -24,6 +26,12 @@ class Menu
       else
         puts "Thanks for using Fake DB Register"
       end
+    elsif option == 3
+      clt.choose_client
+    elsif option == 4
+      clt.delete_a_client
+    else
+      puts "Thanks for using Fake DB Register"
     end
   end
 
