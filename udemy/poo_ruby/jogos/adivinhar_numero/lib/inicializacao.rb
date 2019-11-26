9a7a4830159d40efb397c597a9c64d8862b5e88a
abrandao <andersonbrandaolustosa@gmail.com>
2019-11-26 10:08:21 -0300

    gem para terminal

--- udemy/poo_ruby/jogos/adivinhar_numero/lib/inicializacao.rb
+++ udemy/poo_ruby/jogos/adivinhar_numero/lib/inicializacao.rb
@@ -1,11 +1,27 @@
+require 'tty-cursor'
+require 'tty-spinner'
+
 class Inicializacao
 
   def self.inicializando
+    system('clear')
+
+    cursor = TTY::Cursor
+    print cursor.move_to(15, 20)
+
+    spinner = TTY::Spinner.new
+    spinner = TTY::Spinner.new("[:spinner] Loading ...", format: :pulse_2)
+    spinner.auto_spin
+    sleep(5)
+    spinner.stop('Done!')
+
     print "Inicializando."
     4.times do |i|
       sleep 1
       print "."
-    end
+    end  
+    puts "."
+
+    system('clear')
   end
-  puts "."
 end
