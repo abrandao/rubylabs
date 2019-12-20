8c17fc65bec71a56f12685678ee61cb1de2a48c2
abrandao <andersonbrandaolustosa@gmail.com>
2019-12-20 11:42:35 -0300

    Subject implicito e explicito

--- udemy/tdd/spec/calculator/calculator_spec.rb
+++ udemy/tdd/spec/calculator/calculator_spec.rb
@@ -1,23 +1,22 @@
 require 'calculator'
 
-describe 'Classe Calculadora' do
-
-  subject(:calc) { Calculator.new() }
+describe Calculator, "Sobre a calculadora" do
 
   context '#sum' do
     it 'with positive numbers' do
-      result = calc.sum(5,6)
+      result = subject.sum(5,6)
       expect(result).to eq(11)
     end
 
     it 'with negative numbers' do
-      result = calc.sum(-5,-6)
+      result = subject.sum(-5,-6)
       expect(result).to eq(-11)
     end
 
     it 'with negative and positive numbers' do
-      result = calc.sum(-5,7)
+      result = subject.sum(-5,7)
       expect(result).to eq(2)
     end
   end
+
 end
