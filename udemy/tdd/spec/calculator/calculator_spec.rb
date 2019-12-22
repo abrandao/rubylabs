158e599b594b1d4de2113524d3573b0e87336c87
abrandao <andersonbrandaolustosa@gmail.com>
2019-12-22 11:39:02 -0300

    Matchers equal, be, eql and eq

--- udemy/tdd/spec/calculator/calculator_spec.rb
+++ udemy/tdd/spec/calculator/calculator_spec.rb
@@ -5,7 +5,7 @@ describe Calculator, "Sobre a calculadora" do
   context '#sum' do
     it 'with positive numbers' do
       result = subject.sum(5,6)
-      expect(result).to eq(11)
+      expect(result).to eq(11)      
     end
 
     it 'with negative numbers' do
