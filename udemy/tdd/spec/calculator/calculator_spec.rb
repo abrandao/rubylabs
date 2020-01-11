b2a4779cd1edd93cf4859a6eef6b8cff9dcefc6b
abrandao <andersonbrandaolustosa@gmail.com>
2020-01-11 18:43:13 -0300

    matchers de erros

--- udemy/tdd/spec/calculator/calculator_spec.rb
+++ udemy/tdd/spec/calculator/calculator_spec.rb
@@ -2,10 +2,19 @@ require 'calculator'
 
 describe Calculator, "Sobre a calculadora" do
 
+  context '#div' do
+    it 'divide by 0' do
+      expect{subject.div(3, 0)}.to raise_error(ZeroDivisionError)
+      expect{subject.div(3, 0)}.to raise_error("divided by 0")
+      expect{subject.div(3, 0)}.to raise_error(ZeroDivisionError, "divided by 0")
+      expect{subject.div(3, 0)}.to raise_error(/divided/)
+    end
+  end
+
   context '#sum' do
     it 'with positive numbers' do
       result = subject.sum(5,6)
-      expect(result).to eq(11)      
+      expect(result).to eq(11)
     end
 
     it 'with negative numbers' do
