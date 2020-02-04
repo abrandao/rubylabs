a3e437920af35a4487f77c76450e152c11f1f743
abrandao <andersonbrandaolustosa@gmail.com>
2020-02-04 19:10:47 -0300

    Some updates

---
+++ desafio/app/question_2.rb
@@ -0,0 +1,28 @@
+input = [
+  { name: 'Maria Neusa de Aparecida',
+  cpf: '97905796671',
+  state: 'Sao Paulo',
+  value: '1234' },
+  { name: 'Ricardo Fontes',
+  cpf: '44010762900',
+  state: 'Rio Grande do Sul',
+  value: '567' }
+  ]
+
+def solucao(input)
+  i = 0
+  while i < input.length
+    print input[i][:name][0..10].ljust(11)
+    print input[i][:cpf][0..10].ljust(11)
+    print input[i][:state][0..10].ljust(11)
+    print input[i][:value][0..10].ljust(11)
+    puts "\n"
+    i += 1
+  end
+end
+
+solucao(input)
+
+z = File.readlines("app/format-1.yaml")[0].delete! ":"
+
+print input[0][z][0..10].ljust(11)
---
+++ desafio/app/rascunho.rb
@@ -0,0 +1,49 @@
+
+puts "------------------------------"
+puts " "
+
+a = File.readlines("app/format-1.yaml")[0].delete! ":"
+puts "oi"
+
+b = ":" + a
+puts b
+c = ":cpf"
+
+#puts input[1]['#{b}'][0..10].ljust(11)
+
+puts File.readlines("app/format-1.yaml")
+puts File.readlines("app/format-1.yaml")[0]
+puts File.readlines("app/format-1.yaml")[1]
+
+puts " "
+puts "------------------------------"
+puts " "
+
+Hash[*File.readlines('app/format-1.yaml')]
+puts Hash
+puts input
+
+puts " "
+puts "------------------------------"
+puts " "
+
+x = File.readlines("app/format-1.yaml
+  
+
+
+
+
+z = File.readlines("app/format-1.yaml")[0].delete! ":"
+puts z
+puts " "
+x = z.to_sym
+puts x
+print input[0]['#{x}'][0..10].ljust(11)
+
+
+puts " "
+x = "value".to_sym
+puts x
+print input[0][x][0..10].ljust(11)
+puts "------------------------------"
+puts " "
