69ba613064361802e563395c69cd01852fc9728b
abrandao <andersonbrandaolustosa@gmail.com>
2020-04-04 10:22:37 -0300

    Conversão entre tipos

---
+++ udemy/web_design/JAVASCRIPT/lógica de programação/conversao_entre_tipos.js
@@ -0,0 +1,8 @@
+// Pedir para o usuário digitar um número
+var a = parseFloat(prompt("Digite um número: "));
+
+// Pedir ao usuário que digite um outro número
+var b = parseFloat(prompt("Digite outro número: "));
+
+// Mostrar a soma desses dois números
+alert(a + b);
