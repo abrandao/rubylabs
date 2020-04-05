01a97ec137506c32c4741eb3912ab804facf63e9
abrandao <andersonbrandaolustosa@gmail.com>
2020-04-05 10:21:51 -0300

    Operadores aritméticos

---
+++ udemy/web_design/JAVASCRIPT/lógica de programação/scripts.js
@@ -0,0 +1,17 @@
+function exercicio_operadores() {
+  //pedir para o usuario digitar um numero e converter em numero inteiro
+  var n1 = parseInt(prompt("Digite um número inteiro:"));
+
+  //pedir para o usuario digitar outro numero e converter em numero inteiro
+  var n2 = parseInt(prompt("Digite outro número inteiro:"));
+
+  // Mostrar um alert com o seguinte texto:
+  // O resto da divisão de (n1) por (n2) é: (resultado)
+  res = n1 / n2;
+  alert("O resto da divisão de " + n1 + " por " + n2 + " é: " + res);
+
+  // Mostrar em um alert o seguinte texto:
+  // O número (n1) elevado a (n2) é: (resultado)
+  resexpo = n1 ** n2;
+  alert("O número " + n1 + " elevado a " + n2 + " é: " + resexpo);
+}
