cd05d1b0d7d14f721861653912a3e93c55d7cf05
abrandao <andersonbrandaolustosa@gmail.com>
2020-04-12 08:24:29 -0300

    If... else

--- udemy/web_design/JAVASCRIPT/lógica de programação/scripts.js
+++ udemy/web_design/JAVASCRIPT/lógica de programação/scripts.js
@@ -1,3 +1,4 @@
+// EXERCÍCIO OPERADORES
 function exercicio_operadores() {
   //pedir para o usuario digitar um numero e converter em numero inteiro
   var n1 = parseInt(prompt("Digite um número inteiro:"));
@@ -7,11 +8,38 @@ function exercicio_operadores() {
 
   // Mostrar um alert com o seguinte texto:
   // O resto da divisão de (n1) por (n2) é: (resultado)
-  res = n1 / n2;
+  res = n1 % n2;
   alert("O resto da divisão de " + n1 + " por " + n2 + " é: " + res);
-
+  
   // Mostrar em um alert o seguinte texto:
   // O número (n1) elevado a (n2) é: (resultado)
   resexpo = n1 ** n2;
   alert("O número " + n1 + " elevado a " + n2 + " é: " + resexpo);
+}
+
+
+// OPERADORES DE COMPARAÇÃO PARTE 2
+function operadores_de_comparacao_parte2() {
+  var n1 = parseFloat(prompt('Digite um número: '));
+  var n2 = parseFloat(prompt('Digite outro número: '));
+
+  var media = (n1 + n2) / 2;
+  alert(media > 5);
+}
+
+
+// CONTROLE DE DECISÕES
+function controle_de_decisoes() {
+  var n1 = parseFloat(prompt('Digite um número: '));
+  var n2 = parseFloat(prompt('Digite outro número: '));
+
+  var media = (n1 + n2) / 2;
+  alert(media > 5);
+
+  if(media > 5) {
+    alert("Você foi aprovado!");
+  } else {
+    alert("Você não foi aprovado...");
+  }
+  
 }
