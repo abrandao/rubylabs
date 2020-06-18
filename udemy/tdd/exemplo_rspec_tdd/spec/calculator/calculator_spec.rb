c64ad8fca107443bf783a4a5b24d17101d0dc7e0
abrandao <andersonbrandaolustosa@gmail.com>
2020-06-18 09:21:23 -0300

    Subjects implícitos

--- udemy/tdd/exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
+++ udemy/tdd/exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -1,26 +1,28 @@
 require 'calculator'
 
-describe 'Classe Calculadora' do
+describe Calculator, 'Classe Calculadora' do
 
   subject(:calc) { Calculator.new() }
 
   context '#sum' do
+  
     it 'with positive numbers' do
       calc = Calculator.new
       result = subject.sum(5, 7)
       expect(result).to eq(12)
     end
 
-    specify 'with negative and positive numbers' do
+    it 'with negative and positive numbers' do
       calc = Calculator.new
       result = subject.sum(-5, 7)
       expect(result).to eq(2)
     end
 
-    specify 'with negative numbers' do
+    it 'with negative numbers' do
       calc = Calculator.new
       result = subject.sum(-5, -7)
       expect(result).to eq(-12)
     end
+
   end
 end
