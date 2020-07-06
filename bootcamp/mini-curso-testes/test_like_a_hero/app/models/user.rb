ad69e36b57460ba215e069fceae3faa402cf3f88
abrandao <andersonbrandaolustosa@gmail.com>
2020-07-06 10:41:00 -0300

    Testing models

--- bootcamp/mini-curso-testes/test_like_a_hero/app/models/user.rb
+++ bootcamp/mini-curso-testes/test_like_a_hero/app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   enum kind: [:knight, :wizard]
-  validates :level, numericality: { greater_than: 0, less_than_or_equal: 99 }
+  validates :level, numericality: { greater_than: 0, less_than_or_equal_to: 99 }
 
   def title
-    "#{self.kind} #{self.nickname} ##{self.level}"
+    "#{self.nickname} #{self.kind} ##{self.level}"
   end
 
 end
