eb2d33151900b90355896774298f525bb839f145
abrandao <andersonbrandaolustosa@gmail.com>
2020-07-10 09:22:20 -0300

    Gem FFaker

--- bootcamp/mini-curso-testes/test_like_a_hero/spec/models/user_spec.rb
+++ bootcamp/mini-curso-testes/test_like_a_hero/spec/models/user_spec.rb
@@ -3,12 +3,26 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   
   it 'is invalid if the level is not between 1 and 99' do
-    expect(User.create(nickname: "Chronos", kind: :wizard, level: 100)).to_not be_valid
+    # Using gem ffaker
+    nickname = FFaker::Name.first_name
+    kind = %i[knight wizard].sample
+    level = FFaker::Random.rand(100..9999)
+    user = User.new(nickname: nickname, kind: kind, level: level)
+    
+    expect(user).to_not be_valid
+    
+    # Simple method
+    #expect(User.create(nickname: "Chronos", kind: :wizard, level: 100)).to_not be_valid
   end
 
   it 'returns the correct hero title' do
-    user = User.create(nickname: 'Chronos', kind: :wizard, level: 1)
-    expect(user.title).to eq('Chronos wizard #1')
+    # Using gem ffaker
+    nickname = FFaker::Name.first_name
+    kind = %i[knight wizard].sample
+    level = FFaker::Random.rand(1..99)
+
+    user = User.create(nickname: nickname, kind: kind, level: level)
+    expect(user.title).to eq("#{nickname} #{kind} ##{level}")
   end
 
 end
