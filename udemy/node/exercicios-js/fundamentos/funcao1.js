b50dbacca64d514cf4dca5258bfdb5082e7d7759
abrandao <anderson.brandao@gespag.net.br>
2020-09-17 08:51:56 -0300

    Exemplos let e var

---
+++ udemy/node/exercicios-js/fundamentos/funcao1.js
@@ -0,0 +1,15 @@
+function imprimirSoma(a, b) {
+  console.log(a + b)
+}
+imprimirSoma(2, 3)
+imprimirSoma(2)
+imprimirSoma(2, 3, 5, 7, 8)
+imprimirSoma()
+
+// Funçao com retorno
+function soma(a, b = 1) {
+  return a + b
+}
+
+console.log(soma(2, 3))
+console.log(soma(2))
