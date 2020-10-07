2a587b9f5a6466a9e006d922d8fec788402b6958
abrandao <brandao@rubiti.com>
2020-10-07 11:06:59 -0300

    Matchers composicao, all and be_within lessons

---
+++ udemy/tdd/exemplo_rspec_tdd/spec/matchers/be_within/be_within_spec.rb
@@ -0,0 +1,7 @@
+describe 'be_within' do
+  it { expect(12.5).to be_within(0.5).of(12) }
+  it { expect([11.6, 12.1, 12.4]).to all (be_within(0.5).of(12))}
+end
+
+# delta 0.5
+# Between 11.5 and 12.5
