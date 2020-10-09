8d560ec5cc7ceb344fee48dc02998ac4e2c78cad
abrandao <brandao@rubiti.com>
2020-10-09 12:04:01 -0300

    Composição, Atributos, satisfy and helpers

--- udemy/tdd/exemplo_rspec_tdd/spec/matchers/atributos/atributos_spec.rb
+++ udemy/tdd/exemplo_rspec_tdd/spec/matchers/atributos/atributos_spec.rb
@@ -1,17 +1,50 @@
 require 'pessoa'
 
 describe 'Atributos' do
-  it 'have_attributes' do
-    pessoa = Pessoa.new
-    pessoa.nome = "Anderson"
-    pessoa.idade = 38
-    expect(pessoa).to have_attributes(nome: "Anderson", idade: 38)
+
+  before(:suite) do
+    puts ">>>>>>>>> ANTES de toda a suíte de testes"
+  end
+
+  after(:suite) do
+    puts ">>>>>>>>> DEPOIS de toda a suíte de testes"
+  end
+
+  before(:context) do
+    puts ">>>>>>>>> ANTES de cada teste"
+  end
+
+  after(:all) do
+    puts ">>>>>>>>> DEPOIS de cada teste"
+  end
+
+  before(:each) do
+    @pessoa = Pessoa.new
+    puts ">>>>>>>> ANTES de CADA teste"
+  end
+
+  after(:each) do
+    @pessoa.nome = "Sem nome"
+    puts ">>>>>>>>> #{@pessoa.inspect}"
+    puts ">>>>>>>> DEPOIS de CADA teste"
   end
 
   it 'have_attributes' do
-    pessoa = Pessoa.new
-    pessoa.nome = "Anderson"
-    pessoa.idade = 38
-    expect(pessoa).to have_attributes(nome: a_string_starting_with("A"), idade: (a_value >= 38))
+    @pessoa.nome = "Anderson"
+    @pessoa.idade = 38
+    expect(@pessoa).to have_attributes(nome: "Anderson", idade: 38)
   end
+
+  it 'have_attributes' do
+    @pessoa.nome = "Anderson"
+    @pessoa.idade = 38
+    expect(@pessoa).to have_attributes(nome: a_string_starting_with("A"), idade: (a_value >= 38))
+  end
+
+  it 'have_attributes' do
+    @pessoa.nome = "Jose"
+    @pessoa.idade = 23
+    expect(@pessoa).to have_attributes(nome: a_string_starting_with("J"), idade: (a_value >= 20))
+  end
+  
 end
