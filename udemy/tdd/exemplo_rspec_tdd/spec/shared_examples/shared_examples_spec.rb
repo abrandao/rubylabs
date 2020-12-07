124ce894f559a0eab9defd241c6bd050a7b68925
abrandao <brandao@rubiti.com>
2020-12-07 15:24:01 -0300

    aggregate tests correction

--- udemy/tdd/exemplo_rspec_tdd/spec/shared_examples/shared_examples_spec.rb
+++ udemy/tdd/exemplo_rspec_tdd/spec/shared_examples/shared_examples_spec.rb
@@ -10,9 +10,9 @@ end
 describe 'Pessoa' do
   subject(:pessoa) { Pessoa.new }
 
-  it_behaves_like 'status', :feliz
+  include_examples 'status', :feliz
   it_behaves_like 'status', :triste
-  it_behaves_like 'status', :contente
+  it_should_behave_like 'status', :contente
   
 #  it 'feliz!' do
 #    pessoa.feliz!
