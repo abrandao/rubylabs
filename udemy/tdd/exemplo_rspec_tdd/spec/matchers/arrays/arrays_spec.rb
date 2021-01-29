a6d2c589092c076586d6fc96d1edcb6921017f6f
abrandao <brandao@rubiti.com>
2021-01-29 09:08:06 -0300

    tag filters

--- udemy/tdd/exemplo_rspec_tdd/spec/matchers/arrays/arrays_spec.rb
+++ udemy/tdd/exemplo_rspec_tdd/spec/matchers/arrays/arrays_spec.rb
@@ -1,6 +1,6 @@
 RSpec::Matchers.define_negated_matcher :exclude, :include
 
-describe Array.new([1, 2, 3]), "Array" do
+describe Array.new([1, 2, 3]), "Array", type: 'collection' do
   
   it '#include' do
     expect(subject).to include(2)
@@ -13,7 +13,7 @@ describe Array.new([1, 2, 3]), "Array" do
     expect(subject).to match_array([1, 2, 3])
   end
 
-  it '#contain_exactly' do
+  it '#contain_exactly', :slow do
     expect(subject).to contain_exactly(1, 2, 3)
   end
 
