978be7c72d42c1aaf70c2502daeaa2b623f868dc
abrandao <brandao@rubiti.com>
2021-03-12 09:39:19 -0300

    Sobreescrevedo atributos e aliases para fábricas

--- udemy/tdd/test_app/spec/models/customer_spec.rb
+++ udemy/tdd/test_app/spec/models/customer_spec.rb
@@ -2,10 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
   
-  it '#full_name' do
-   customer = create(:customer)
-   expect(customer.full_name).to start_with("Sr. ")
+  it '#full_name - Sobrescrevendo Atributo' do
+   customer = create(:customer, name: "Anderson Brandão")
+   expect(customer.full_name).to start_with("Sr. Anderson Brandão")
   end
+
+  it '#full_name' do
+    customer = create(:user) # ou create(:customer)
+    expect(customer.full_name).to start_with("Sr. ")
+   end
 
   it { expect {create(:customer) }.to change{Customer.all.size}.by(1) }
 end
