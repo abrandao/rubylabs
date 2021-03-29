f51a31240f323fe9c6e541a2d05c30ea48d45819
abrandao <brandao@rubiti.com>
2021-03-29 18:46:19 -0300

    HTTParty

--- udemy/tdd/test_app/spec/factories/orders.rb
+++ udemy/tdd/test_app/spec/factories/orders.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :order do
     sequence(:description) { |n| "Pedido número - #{n}" }
     
-    #association :customer, factory: :customer
+    association :customer, factory: :customer
     customer
   end
 end
