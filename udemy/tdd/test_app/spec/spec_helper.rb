3c1b731ea8a30f043af036f71e5e506b27275dea
brandao <abrandao>
2021-04-04 10:18:59 -0300

    vcr

--- udemy/tdd/test_app/spec/spec_helper.rb
+++ udemy/tdd/test_app/spec/spec_helper.rb
@@ -1,6 +1,11 @@
 #webmock gem
 require 'webmock/rspec'
 
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+end
+
 RSpec.configure do |config|
   
   #FactoryBot Lint
