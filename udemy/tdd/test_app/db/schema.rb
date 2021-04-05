37583052b7b975f793f472cbbb897451187c1aca
brandao <abrandao>
2021-04-05 17:03:45 -0300

    VCR sensitive data

--- udemy/tdd/test_app/db/schema.rb
+++ udemy/tdd/test_app/db/schema.rb
@@ -1,4 +1,16 @@
-ActiveRecord::Schema.define(version: 2021_03_22_142831) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_03_09_155339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -9,17 +21,8 @@ ActiveRecord::Schema.define(version: 2021_03_22_142831) do
     t.boolean "vip"
     t.integer "days_to_pay"
     t.string "gender"
-    t.datetime "updated_at", precision: 6, null: false
-    t.datetime "created_at", precision: 6, null: false
-  end
-
-  create_table "orders", force: :cascade do |t|
-    t.string "description"
-    t.bigint "customer_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["customer_id"], name: "index_orders_on_customer_id"
   end
 
-  add_foreign_key "orders", "customers"
 end
