3f5e696c1056fb7c7bf55b2452b84dd91a42b874
brandao <abrandao>
2021-05-20 10:40:38 -0300

    PHP lessons

---
+++ PHP/public/operadores/exemplo-08.php
@@ -0,0 +1,13 @@
+<?php
+
+  $eComercial = (10 + 3) / 2 > 5 && 10 + 5 < 3;
+
+  var_dump($eComercial);
+
+  echo "<br>";
+
+  $pipePipe = (10 + 3) / 2 > 5 || 10 + 5 < 3;
+
+  var_dump($pipePipe);
+
+?>
